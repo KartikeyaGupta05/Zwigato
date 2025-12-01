231f669eeebdddfc767029e5ed6096fcacec0849
Kartikeyagupta5 <kartikeyagupta998@gmail.com>
2025-12-01 21:25:28 +0530

    fix some codes after deploying

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ const server = http.createServer(app);
 const port = process.env.PORT || 5000;
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://zwigato-drab.vercel.app",
     credentials: true,
     methods: ["POST", "GET"],
   },
@@ -29,7 +29,7 @@ app.set("io", io);
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://zwigato-drab.vercel.app",
     credentials: true,
   })
 );
